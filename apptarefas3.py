ed9cc6ac0f0d83d9b1f2923ec103a7bef8da11f5
Marcos Santos <112584092+Nihilatyk@users.noreply.github.com>
2023-11-09 14:23:16 -0300

    Update apptarefas3.py

--- apptarefas3.py
+++ apptarefas3.py
@@ -10,6 +10,14 @@ def is_valid_date(date):
     except ValueError:
         return False
 
+def print_tasks(tasks):
+    if tasks:
+        print("\nTarefas:")
+        for i, task in enumerate(tasks, 1):
+            print(f"{i} - Descrição: {task['description']}, Data de Vencimento: {task['due_date']}, Prioridade: {task['priority']}")
+    else:
+        print("Nenhuma tarefa cadastrada.")
+
 def main():
     tasks = []
 
@@ -17,7 +25,9 @@ def main():
         print("\nEscolha uma opção:")
         print("1 - Cadastrar tarefa")
         print("2 - Listar tarefas")
-        print("3 - Sair")
+        print("3 - Alterar tarefa")
+        print("4 - Apagar tarefa")
+        print("5 - Sair")
 
         choice = input("Digite o número da opção: ")
 
@@ -33,14 +43,36 @@ def main():
                 print("Prioridade ou data inválida. Tarefa não cadastrada.")
 
         elif choice == "2":
-            if tasks:
-                print("\nTarefas:")
-                for i, task in enumerate(tasks, 1):
-                    print(f"{i} - Descrição: {task['description']}, Data de Vencimento: {task['due_date']}, Prioridade: {task['priority']}")
-            else:
-                print("Nenhuma tarefa cadastrada.")
+            print_tasks(tasks)
 
         elif choice == "3":
+            print_tasks(tasks)
+            task_index = int(input("Digite o número da tarefa que deseja alterar: ")) - 1
+
+            if 0 <= task_index < len(tasks):
+                description = input("Digite a nova descrição da tarefa: ")
+                due_date = input("Digite a nova data de vencimento (AAAA-MM-DD): ")
+                priority = input("Digite a nova prioridade (alta, média ou baixa): ")
+
+                if is_valid_priority(priority) and is_valid_date(due_date):
+                    tasks[task_index] = {"description": description, "due_date": due_date, "priority": priority}
+                    print("Tarefa alterada com sucesso!")
+                else:
+                    print("Prioridade ou data inválida. Tarefa não alterada.")
+            else:
+                print("Número de tarefa inválido.")
+
+        elif choice == "4":
+            print_tasks(tasks)
+            task_index = int(input("Digite o número da tarefa que deseja apagar: ")) - 1
+
+            if 0 <= task_index < len(tasks):
+                del tasks[task_index]
+                print("Tarefa apagada com sucesso!")
+            else:
+                print("Número de tarefa inválido.")
+
+        elif choice == "5":
             print("Saindo do aplicativo.")
             break
 
@@ -49,5 +81,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
 
